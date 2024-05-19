d6de68cc0fb873daa23a5d4019a6ee0034e169d2
Ashish <ashishpandey2062@gmail.com>
2024-05-19 18:47:53 +0545

    use_auth_set

--- Project_NyanoGhar/settings.py
+++ Project_NyanoGhar/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3!xioaguqs5fe9qeub%t#yu#+80ff)@s#oag*dyt0j&4s&zc2("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nyanoghar@gmail.com', 'www.nyanoghar@gmail.com', '127.0.0.1']
+
 
 
 # Application definition
--- user_auth_app/views.py
+++ user_auth_app/views.py
@@ -28,9 +28,16 @@ def signup(request):
 
 @api_view(['POST'])
 def login(request):
-    return Response({})
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return Response({'token': token.key, 'user': serializer.data})
 
 
 @api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def test_token(request):
-    return Response({})
+    return Response("passed!")
